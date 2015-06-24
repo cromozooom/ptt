7cb38000dbfb78abd2541af0f978c27e836204fd
cromozooom <cromozooom@gmail.com>
2015-06-24 16:47:07 +0200

    add files

---
+++ ppt-slug/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package ptt
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'ppt-slug' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'ppt-slug' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'ppt-slug' ), 'ppt-slug', '<a href="http://www.solopx.com" rel="designer">solopx</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
